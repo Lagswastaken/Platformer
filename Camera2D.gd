9b6fd86f71680a676d46ed47975f4c460320ee4f
Lagswastaken <conkni25@kingshigh.school.nz>
2022-08-05 09:57:34 +1200

    Door and Button
    Added the door and button code and started on the spinning trap

---
+++ Camera2D.gd
@@ -0,0 +1,15 @@
+extends Camera2D
+var zoomed = false
+onready var target = get_parent().get_node("Player")
+onready var center = get_viewport_rect().size/2
+
+func _process(delta):
+	if zoomed:
+		zoom = Vector2(0.3,0.3)
+		position =  target.global_position
+	else:
+		zoom = Vector2(1,1)
+		position = center
+		
+	if Input.is_action_just_pressed("zoom"):
+		zoomed = not zoomed
