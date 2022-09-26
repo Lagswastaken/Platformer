1698653cb27b5c6a980797ddb7e44f4f15a612ca
Lagswastaken <conkni25@kingshigh.school.nz>
2022-09-27 10:39:00 +1300

    .

--- Scripts/DoorAndButton.gd
+++ Scripts/DoorAndButton.gd
@@ -4,9 +4,12 @@ extends Node2D
 func _ready():
 	$AnimationPlayer.play("ButtonUp")
 	$AnimationPlayer.play("DoorClosed")
-
+	
+var dooropen = false
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("Player"):
-		$AnimationPlayer.play("ButtonDown")
-		$AnimationPlayer.play("DoorOpen")
+		if dooropen == false:
+			$AnimationPlayer.play("ButtonDown")
+			$AnimationPlayer.play("DoorOpen")
+			dooropen = true
